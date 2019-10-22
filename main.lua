9b6a3c4e08c10f5d16ec0e76e3ad87d777ceb353
1337FEET <hayden.grham@gmail.com>
2019-10-22 16:24:11 +0200

    Everything prepared to begin distance calculation code

--- main.lua
+++ main.lua
@@ -6,9 +6,11 @@
 
 require "Tests.lunatest"
 require "dataLoader"
+require "distanceCalculator"
 
 --Test suites
 lunatest.suite("Tests.Suites.LoadData")
+lunatest.suite("Tests.Suites.CalculateDistance")
 
 --Main function (to be moved)
 fileName = "knn.csv"

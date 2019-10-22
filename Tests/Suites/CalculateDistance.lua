d9ad9ffc7b3b6ab085bcf6de03b6975d59c63022
1337FEET <hayden.grham@gmail.com>
2019-10-22 21:58:00 +0200

    finished manhattan test

--- Tests/Suites/CalculateDistance.lua
+++ Tests/Suites/CalculateDistance.lua
@@ -21,16 +21,27 @@ function test_cosineSimilarity()
 end
 
 function test_euclideanDistance()
+  function round(num, pl)
+    return math.floor((num*math.pow(10,pl)+0.5))/math.pow(10,pl)
+  end
   local point1 = {-10, 5}
   local point2 = {3, 6}
   local point3 = {12, 2}
-  local point4 = {5, 9}
-  local point5 = {1, -4}
-  local point6 = {-2, -8}
   local result = true
    --test
-  if round(minkowskiDistance(point1, point2, 2), 3) ~= 13.038 or round(minkowskiDistance(point2, point3, 2), 3) ~= 9.849 then-- or round(minkowskiDistance(point3, point4, 2), 3) ~= 0.623 or round(minkowskiDistance(point4, point5, 2), 3) ~= -0.730 or round(minkowskiDistance(point5, point6, 2), 3) ~= 0.882 or round(minkowskiDistance(point1, point6, 2),3) ~= -0.217 then
+  if round(minkowskiDistance(point1, point2, 2), 3) ~= 13.038 or round(minkowskiDistance(point2, point3, 2), 3) ~= 9.849 then    
     result = nil
   end 
   assert_not_nil(result, "euclidean distance calculation failed")
+end
+
+function test_manhattanDistance()
+  local point1 = {5, 9}
+  local point2 = {1, -4}
+  local point3 = {-2, -8}
+  local result = true
+  if minkowskiDistance(point1,point2,1) ~= 17 or minkowskiDistance(point2, point3, 1) ~= 7 or minkowskiDistance(point1, point3, 1) ~= 24 then
+    result = nil
+  end
+  assert_not_nil(result, "manhattan distance calculation failed")
 end
--- distanceCalculator.lua
+++ distanceCalculator.lua
@@ -7,7 +7,12 @@ function cosineSimilarity(point1, point2)
     return pnt1[1]*pnt2[1] + pnt1[2]*pnt2[2]
   end
 
-  distance = dotProduct(point1, point2)/(vectorMagnitude(point1)*vectorMagnitude(point2))
+  local distance = dotProduct(point1, point2)/(vectorMagnitude(point1)*vectorMagnitude(point2))
   
+  return distance
+end
+--p value = 1 for manhattan distance. p = 2 for euclidean etc.
+function minkowskiDistance(point1, point2, p)
+  local distance = math.pow(math.pow(math.abs(point1[1] - point2[1]), p) +  math.pow(math.abs(point1[2] - point2[2]), p), 1/p)
   return distance
 end
